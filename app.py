928f7889f3da841d89a382cdb462dc7bb0c73fba
shailja-07 <2004shailja@gmail.com>
2025-01-18 20:32:52 +0530

    change3

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ async def detect_face_and_eyes(file: UploadFile = File(...)):
         nparr = np.frombuffer(contents, np.uint8)
         image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-        
+       
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         
@@ -30,28 +30,34 @@ async def detect_face_and_eyes(file: UploadFile = File(...)):
 
             
             eyes = eye_cascade.detectMultiScale(roi_gray)
-            eyes = sorted(eyes, key=lambda e: e[0]) 
+            eyes = sorted(eyes, key=lambda e: e[0])  
 
             if len(eyes) >= 2:
-                (ex1, ey1, ew1, eh1) = eyes[0] 
+                
+                (ex1, ey1, ew1, eh1) = eyes[0]
                 left_eye = roi_color[ey1:ey1+eh1, ex1:ex1+ew1]
-                enlarged_left_eye = cv2.resize(left_eye, (2*ew1, 2*eh1))  
+                enlarged_left_eye = cv2.resize(left_eye, (ew1 * 2, eh1 * 2))  
 
-                (ex2, ey2, ew2, eh2) = eyes[1] 
+                (ex2, ey2, ew2, eh2) = eyes[1]
                 right_eye = roi_color[ey2:ey2+eh2, ex2:ex2+ew2]
-                enlarged_right_eye = cv2.resize(right_eye, (2*ew2, 2*eh2))  
+                enlarged_right_eye = cv2.resize(right_eye, (ew2 * 2, eh2 * 2))  
 
-               
-                roi_color[ey1:ey1+eh1, ex1:ex1+ew1] = enlarged_left_eye
-                roi_color[ey2:ey2+eh2, ex2:ex2+ew2] = enlarged_right_eye
+                
+                enlarged_left_eye = cv2.resize(enlarged_left_eye, (ew1 * 2, eh1 * 2))
+                enlarged_right_eye = cv2.resize(enlarged_right_eye, (ew2 * 2, eh2 * 2))
+
+                
+                roi_color[ey1:ey1+eh1*2, ex1:ex1+ew1*2] = enlarged_left_eye
+                roi_color[ey2:ey2+eh2*2, ex2:ex2+ew2*2] = enlarged_right_eye
 
                 
                 _, img_bytes = cv2.imencode('.jpg', image)
                 img_io = io.BytesIO(img_bytes.tobytes())
+                
+                
                 return StreamingResponse(img_io, media_type="image/jpeg")
 
         return {"message": "No face or eyes detected."}
 
     except Exception as e:
         return {"error": str(e)}
-
